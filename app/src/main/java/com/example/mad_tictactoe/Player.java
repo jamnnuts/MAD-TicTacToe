2559f5b8471e7950ed0ff1eb58e570ad36e9f3df
dadardark <jacobarvino@gmail.com>
2023-09-14 23:36:45 +0800

    -Added player's turn to GameBoardFrag4x4.java
    TODO:
    Create recyclerview for avatar drawables
    Create drawableID class field in Player.java
    Fix win% not showing correct number

--- app/src/main/java/com/example/mad_tictactoe/Player.java
+++ app/src/main/java/com/example/mad_tictactoe/Player.java
@@ -9,6 +9,7 @@ public class Player{
     public int wins;
     public int losses;
     public int draws;
+    public int avatarID;.
 
     public Player(String initialName) {
         this.playerName = initialName;
@@ -16,6 +17,7 @@ public class Player{
         this.wins = 0;
         this.losses = 0;
         this.draws = 0;
+        this.avatarID = 0;
     }
 
     public String getPlayerName() {return playerName;}
@@ -23,9 +25,12 @@ public class Player{
     public int getGamesPlayed() {return gamesPlayed;}
     public int getLosses() {return losses;}
     public int getWins() {return wins;}
+    public int getAvatarID() {return avatarID;}
     public void setPlayerName(String playerName) {this.playerName = playerName;}
     public void setDraws(int draws) {this.draws = draws;}
     public void setGamesPlayed(int gamesPlayed) {this.gamesPlayed = gamesPlayed;}
     public void setLosses(int losses) {this.losses = losses;}
     public void setWins(int wins) {this.wins = wins;}
+    public void setAvatarID(int avatarID) {this.avatarID = avatarID;
+    }
 }
